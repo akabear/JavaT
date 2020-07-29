b82e735c50a2162da2c6cb167e9a69990ec4b17f
akabear <68880382+akabear@users.noreply.github.com>
2020-07-28 20:32:20 -0400

    CountArea

---
+++ Sandbox/src/main/java/LessonA.java
@@ -0,0 +1,11 @@
+public class LessonA {
+
+  public static void main(String[] args) {
+    Rectangle r = new Rectangle(7.0, 5.0);
+    System.out.println("Hello " + calcArea(r));
+  }
+
+  public static double calcArea(Rectangle r) {
+    return r.a * r.b;
+  }
+}  
